83fc32849335bf67e227d314da635e94ee32ad11
Mike Gorski <mgorski@outsellinc.com>
2011-05-16 08:42:14 -0500

    update creating of hash

--- app/models/rating_link.rb
+++ app/models/rating_link.rb
@@ -3,10 +3,9 @@ class RatingLink < ActiveRecord::Base
 	validates_presence_of :for_date, :rating, :user_id, :link_hash
 	validates_uniqueness_of :link_hash
 	validates_uniqueness_of :user_id, :scope => [:for_date, :rating]
-	before_create :setup_link_hash
-	
-	def setup_link_hash
+	before_validation(:on => :create) do
 		self.link_hash = Digest::SHA1.hexdigest("#{user.email}--#{for_date}--#{rating}")
 	end
+	
 end
 	
