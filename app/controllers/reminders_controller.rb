7e31b9c41c151404bf08f65756dbbd4988d1d4a4
Mike Gorski <mgorski@outsellinc.com>
2011-05-16 08:31:52 -0500

    update iphone sheet and rating links

--- app/controllers/reminders_controller.rb
+++ app/controllers/reminders_controller.rb
@@ -1,7 +1,7 @@
 class RemindersController
-	def self.send_daily_reminder(time_for=1.day.ago.to_date)
+	def self.send_daily_reminder(for_date=1.day.ago.to_date)
 		User.find(:all).each do |user|
-			Reminders.daily(user, for_date).deliver unless user.status_logged_for_today?(time_for)
+			Reminders.daily(user, for_date).deliver unless user.status_logged_for_today?(for_date)
 		end
 	end
 	
--- app/models/rating_link.rb
+++ app/models/rating_link.rb
@@ -5,7 +5,6 @@ class RatingLink < ActiveRecord::Base
 	validates_uniqueness_of :user_id, :scope => [:for_date, :rating]
 	before_create :setup_link_hash
 	
-	private
 	def setup_link_hash
 		self.link_hash = Digest::SHA1.hexdigest("#{user.email}--#{for_date}--#{rating}")
 	end
